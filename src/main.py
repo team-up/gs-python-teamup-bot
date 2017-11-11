bc5e63d87934c61403bab27ecc0a3cfa6bd347d0
sorrowbeaver <jkj8790@gmail.com>
2017-11-11 13:50:08 +0900

    이벤트 핸들링을 쓰레드로 처리하도록 수정

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import json
+import threading
 
 import os
 
@@ -14,18 +15,19 @@ with open(file_name) as data_file:
 
 service.login(authentication)
 
+# thread-safe 하다고 나와있긴 하지만 보장이 되는지 고민해 봐야함
+# callback 패턴으로 바꾸는 것 고려
+def handle_event(events):
+    for event in events:
+        if event.chat_event:
+            chat = service.get_chat_summary(event.chat_event.room_index,
+                                            event.chat_event.msg_index)
+
+            if chat and chat.content == "ㅎㅇ":
+                service.post_chat(event.chat_event.room_index, "ㅎㅇㅎㅇ")
+
 while True:
     events = service.get_events()
     if events:
-        for event in events:
-            if event.chat_event:
-                chat = service.get_chat_summary(event.chat_event.room_index,
-                                                event.chat_event.msg_index)
-
-                if chat:
-                    print("채팅 옴 : {}".format(chat.content))
-
-                    if chat.content == "ㅎㅇ":
-                        service.post_chat(event.chat_event.room_index,
-                                          "ㅎㅇㅎㅇ")
+        threading.Thread(target=handle_event, args=(events,)).start()
     time.sleep(service.config['lp_idle_time'])
