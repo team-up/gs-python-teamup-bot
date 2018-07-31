5b0d16001751c85f335b0d230fdb348b0d4ef893
KyoungJoo <jkj8790@estsoft.com>
2018-07-31 18:24:59 +0900

    호스트 수정

--- teamup_service.py
+++ teamup_service.py
@@ -4,9 +4,9 @@ import requests
 
 from event import EventFactory
 
-event_host = 'https://dev-ev.tmup.com'
-auth_host = 'https://dev-auth.tmup.com'
-edge_host = 'https://dev-edge.tmup.com'
+event_host = 'https://ev.tmup.com'
+auth_host = 'https://auth.tmup.com'
+edge_host = 'https://edge.tmup.com'
 
 logger = logging.getLogger("teamup-bot")
 
